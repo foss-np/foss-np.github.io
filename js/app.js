a93a3ab334294cbc7450bb7863896eef8ae47601
Avinash Kundaliya <avinash@avinash.com.np>
2013-03-10 18:31:59 +0545

    adding contributor images

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
-function mainCtrl($scope,$rootScope,$http){
+var fossnp = angular.module('fossnp',[]);
+
+fossnp.config(function($routeProvider) {
+	$routeProvider.when('/repo/:repo/:owner',{templateUrl:'contributor.html',controller:repoCtrl})
+});
+
+function mainCtrl($scope, $rootScope, $http, $route, $location, $routeParams){
 	$http.get('https://api.github.com/orgs/foss-np/repos').success(function(data){
-		$scope.repos = data;
-	})
+		$rootScope.repos = data;
+	});
+	$scope.$route = $route;
+	$scope.$location = $location;
+	$scope.$routeParams = $routeParams;
+}
+
+function repoCtrl($scope, $http, $routeParams){
+	console.log('hello World');
+	$http.get('https://api.github.com/repos/'+$routeParams.owner+'/'+$routeParams.repo+'/contributors').success(function(data){
+		$scope.contributors = data;
+	});
 }

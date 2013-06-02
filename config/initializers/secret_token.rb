31e686e4536a7f9e05aa5cc7a29bbb970ee5dd06
Sushil Dahal <sushilthe@gmail.com>
2013-06-02 13:55:03 +0545

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewFoss::Application.config.secret_token = '41ffd5012f3350e0bab1cca4ba9a10e84e733808cdca79e3a7129111d4ef833bf46b403115d57205a528ad188f728cbc1c4037b407e57df1730e84cc7f064d8b'

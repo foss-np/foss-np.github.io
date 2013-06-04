ede220288f4dd53b080c5402a6e7d08f3d9ec05f
Sushil Dahal <sushilthe@gmail.com>
2013-06-05 01:09:12 +0545

    Forum rights modified

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,5 @@
 class TopicsController < ApplicationController
-  before_filter :signed_in_user, only: [ :edit, :update, :destroy]
+  before_filter :signed_in_user, only: [ :new, :create, :edit, :update, :destroy]
   before_filter :admin_user,     only: [ :edit, :update, :destroy]     
   def show
     @topic = Topic.find(params[:id])
